76bfac334d2ee98828b6cdb5af8b325e34d99e0d
pupper emeritus <80730927+pupperemeritus@users.noreply.github.com>
2023-05-11 23:17:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,26 +2,11 @@
 Run this file using python and provide the develop argument."""
 from setuptools import setup
 setup(
-    name="glance-a-lot",
+    name="summarizer",
     version="0.0.1",
-    url="https://github.com/pupperemeritus/glance-a-lot",
+    url="https://github.com/Pavankumarpatale/summarizer",
     license="GPL-3.0",
-    install_requires=["scikit-learn",
-                      "datetime",
-                      "requests",
-                      "youtube-search-python",
-                      "google-search",
-                      "dotenv",
-                      "pymongo",
-                      "discord.py",
-                      "pandas",
-                      "seaborn",
-                      "matplotlib",
-                      "pickle",
-                      "numpy",
-                      "random"
-                      ],
-    packages=["glance-a-lot"],
+    install_requires=[],
     include_package_data=True,
     zip_safe=False
 )
